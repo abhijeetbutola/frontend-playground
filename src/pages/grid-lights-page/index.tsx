482eb5f3f4955a45c8cc7b2ea69b308cadf63b6d
Abhijeet Butola <abhijeet.butola@gmail.com>
2025-04-30 20:06:22 +0530

    grid lights challenge

--- src/pages/grid-lights-page/index.tsx
+++ src/pages/grid-lights-page/index.tsx
@@ -7,17 +7,37 @@ const grid = [
 ];
 
 function GridLightsPage() {
-  const [stack, setStack] = useState<Record<string, boolean>>({});
+  const [stack, setStack] = useState<Map<string, boolean>>(new Map());
+
+  const lightsOff = () => {
+    const interval = setInterval(() => {
+      setStack((prevStack) => {
+        const tempStack = structuredClone(prevStack);
+        const lastKey = Array.from(prevStack.keys()).pop();
+        if (lastKey !== undefined) {
+          tempStack.delete(lastKey);
+        }
+        if (!tempStack.size) clearInterval(interval);
+        return tempStack;
+      });
+    }, 800);
+  };
 
   const handleGridClick = (rowIndex: number, colIndex: number) => {
     const newStack = structuredClone(stack);
 
     const key = `${rowIndex}-${colIndex}`;
 
-    if (newStack[key] || !grid[rowIndex][colIndex]) return;
-    else newStack[key] = true;
+    if (newStack.has(key) || !grid[rowIndex][colIndex]) return;
+    else newStack.set(key, true);
 
     setStack(newStack);
+
+    const lightsCount = grid.flat().reduce((acc, curr) => (acc += curr), 0);
+
+    if (lightsCount === stack.size + 1) {
+      lightsOff();
+    }
   };
 
   return (
@@ -26,18 +46,18 @@ function GridLightsPage() {
       <div>
         <div>
           {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="flex justify-center items-center">
+            <div key={rowIndex} className="flex items-center justify-center">
               {row.map((value, colIndex) => {
-                const color = stack[`${rowIndex}-${colIndex}`]
+                const color = stack.has(`${rowIndex}-${colIndex}`)
                   ? "bg-indigo-600"
                   : value === 0
-                  ? "bg-red-500/70"
-                  : "";
+                    ? "bg-red-500/70"
+                    : "";
 
                 return (
                   <span
                     key={colIndex}
-                    className={`border border-black p-12 bg-gray-500 ${color}`}
+                    className={`border border-black bg-gray-500 p-12 ${color}`}
                     onClick={() => handleGridClick(rowIndex, colIndex)}
                   ></span>
                 );
